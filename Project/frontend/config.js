44ad03f2395cfa127e9591335d95853f37dafd32
Aryan-Garg-dev <aryangarg3010@gmail.com>
2024-03-15 14:44:48 +0530

    update

--- Project/frontend/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = "https://hximvjzbghwhlbasgjjg.supabase.co";
-export const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4aW12anpiZ2h3aGxiYXNnampnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzMzY0NDksImV4cCI6MjAyNTkxMjQ0OX0.bwm9OxaxsX2aFUnhnl9GsVXOWAn3FYcwzbqA5ZrI7zk";
-
-
-
