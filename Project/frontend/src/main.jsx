599843d9fb2604880baddc743aa5a7c79d7867aa
Aryan-Garg-dev <aryangarg3010@gmail.com>
2024-03-15 11:43:50 +0530

    update

--- Project/frontend/src/main.jsx
+++ Project/frontend/src/main.jsx
@@ -8,8 +8,8 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { API_URL, API_KEY } from '../config.js'
 
 const supabase = createClient(
-  API_URL,
-  API_KEY
+  API_URL, //Supabase API-URL
+  API_KEY //Supabase API-KEY
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
